9e534d5f3e8b4a782ac93131724b598311f22efa
尹志祥 <yinzhixiang@zhixiangdeAir.lan>
2022-02-23 22:42:14 +0800

    project init

--- src/main.js
+++ src/main.js
@@ -3,8 +3,13 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import fastClick from 'fastclick'
+import './assets/style/reset.css'
+import './assets/style/border.css'
 
 Vue.config.productionTip = false
+// 解决300ms延迟
+fastClick.attach(document.body)
 
 /* eslint-disable no-new */
 new Vue({
