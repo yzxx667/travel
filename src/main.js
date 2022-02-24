f034d4dfc6139403028771dd9b6cb31773aed74a
尹志祥 <yinzhixiang@zhixiangdeAir.lan>
2022-02-24 11:23:49 +0800

    add header

--- src/main.js
+++ src/main.js
@@ -4,8 +4,9 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 import fastClick from 'fastclick'
-import './assets/style/reset.css'
-import './assets/style/border.css'
+import 'style/reset.css'
+import 'style/border.css'
+import 'style/iconfont.css'
 
 Vue.config.productionTip = false
 // 解决300ms延迟
