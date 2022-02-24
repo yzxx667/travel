83eaa2854dc2f45cca2e410c3a28ebd6fc2f2952
尹志祥 <yinzhixiang@zhixiangdeAir.lan>
2022-02-24 20:31:13 +0800

    add icos

---
+++ src/pages/home/components/Icons.vue
@@ -0,0 +1,140 @@
+<template>
+  <div class="icons">
+    <swiper>
+    <swiper-slide v-for="(page,index) of pages" :key="index">
+      <div class="icon"
+       v-for="item of page"
+       :key="item.id"
+       >
+        <div class="icon-img">
+          <img
+            class="icon-img-content"
+            :src="item.imgUrl"
+            alt=""
+          />
+        </div>
+        <p class="icon-desc">{{item.desc}}</p>
+      </div>
+    </swiper-slide>
+    </swiper>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomeIcons',
+  data () {
+    return {
+      iconList: [{
+        id: '0001',
+        imgUrl: 'http://img1.qunarzz.com/piao/fusion/1611/54/ace00878a52d9702.png',
+        desc: '景点门票'
+      },
+      {
+        id: '0002',
+        imgUrl: 'http://img1.qunarzz.com/piao/fusion/1711/df/86cbcfc533330d02.png',
+        desc: '滑雪季'
+      },
+      {
+        id: '0003',
+        imgUrl: 'http://img1.qunarzz.com/piao/fusion/1710/a6/83f636bd75ae6302.png',
+        desc: '泡温泉'
+      },
+      {
+        id: '0004',
+        imgUrl: 'https://img1.qunarzz.com/piao/fusion/1611/35/2640cab202c41b02.png',
+        desc: '动物园'
+      },
+      {
+        id: '0005',
+        imgUrl: 'http://img1.qunarzz.com/piao/fusion/1611/54/ace00878a52d9702.png',
+        desc: '景点门票'
+      },
+      {
+        id: '0006',
+        imgUrl: 'http://img1.qunarzz.com/piao/fusion/1711/df/86cbcfc533330d02.png',
+        desc: '滑雪季'
+      },
+      {
+        id: '0007',
+        imgUrl: 'http://img1.qunarzz.com/piao/fusion/1710/a6/83f636bd75ae6302.png',
+        desc: '泡温泉'
+      },
+      {
+        id: '0008',
+        imgUrl: 'https://img1.qunarzz.com/piao/fusion/1611/35/2640cab202c41b02.png',
+        desc: '动物园'
+      },
+      {
+        id: '0009',
+        imgUrl: 'https://img1.qunarzz.com/piao/fusion/1611/35/2640cab202c41b02.png',
+        desc: '动物园'
+      }]
+    }
+  },
+  computed: {
+    pages () {
+      const pages = []
+      this.iconList.forEach((item, index) => {
+        const page = Math.floor(index / 8)
+        if (!pages[page]) {
+          pages[page] = []
+        }
+        pages[page].push(item)
+      })
+      return pages
+    }
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+@import '~style/varibles.styl';
+@import '~style/mixins.styl';
+.icons >>> .swiper-container{
+    height: 0
+    padding-bottom: 50%
+}
+.icons {
+  overflow: hidden;
+  height: 0;
+  padding-bottom: 50%;
+
+  .icon {
+    position: relative;
+    overflow: hidden;
+    float: left;
+    width: 25%;
+    height: 0;
+    padding-bottom: 25%;
+
+    .icon-img {
+      position: absolute;
+      top: 0;
+      left: 0;
+      right: 0;
+      bottom: 0.44rem;
+      box-sizing: border-box;
+      padding: 0.1rem;
+
+      .icon-img-content {
+        display: block;
+        margin: 0 auto;
+        height: 100%;
+      }
+    }
+
+    .icon-desc {
+      position: absolute;
+      left: 0;
+      right: 0;
+      bottom: 0;
+      height: 0.44rem;
+      line-height: 0.44rem;
+      color: $darkTextColor;
+      text-align: center;
+      ellipsis()
+    }
+  }
+}
+</style>
