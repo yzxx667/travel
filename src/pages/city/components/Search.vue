faa859bf5401d153262a1b466fa5fd4585e6fb92
尹志祥 <yinzhixiang@zhixiangdeAir.lan>
2022-02-27 10:30:38 +0800

    search-finish

--- src/pages/city/components/Search.vue
+++ src/pages/city/components/Search.vue
@@ -1,12 +1,71 @@
 <template>
-  <div class="search">
-     <input class="search-input" type="text" name="" id="" placeholder="输入城市名或拼音">
+  <div>
+    <div class="search">
+      <input class="search-input"
+       type="text" name=""
+       placeholder="输入城市名或拼音"
+       v-model="keyword">
+    </div>
+    <div class="search-content" ref="search" v-show="keyword">
+      <ul>
+        <li
+        class="search-item border-bottom"
+        v-for="item of list"
+        :key="item.id"
+        >{{item.name}}</li>
+        <li class="search-item border-bottom" v-show="hasNoData">没有找到匹配数据</li>
+      </ul>
+    </div>
   </div>
 </template>
 
 <script>
+import Bscroll from 'better-scroll'
 export default {
-  name: 'CitySearch'
+  name: 'CitySearch',
+  props: {
+    cities: Object
+  },
+  data () {
+    return {
+      keyword: '',
+      list: [],
+      timer: null
+    }
+  },
+  computed: {
+    hasNoData () {
+      return !this.list.length
+    }
+  },
+  watch: {
+    keyword () {
+      if (this.timer) {
+        clearTimeout(this.timer)
+      }
+      if (!this.keyword) {
+        this.list = []
+        return
+      }
+      this.timer = setTimeout(() => {
+        const result = []
+        for (let i in this.cities) {
+          this.cities[i].forEach((value) => {
+            if (value.name.indexOf(this.keyword) > -1 || value.spell.indexOf(this.keyword) > -1) {
+              result.push(value)
+            }
+          })
+        }
+        this.list = result
+      }, 100)
+    }
+  },
+  mounted () {
+    this.scroll = new Bscroll(this.$refs.search)
+  },
+  updated () {
+    this.scroll.refresh()
+  }
 }
 </script>
 
@@ -25,4 +84,18 @@ export default {
             text-align: center
             border-radius: .06rem
             color:#666
+    .search-content
+      z-index 1
+      overflow: hidden
+      position: absolute
+      top:1.58rem
+      left: 0
+      right: 0
+      bottom: 0
+      background: #eee
+      .search-item
+        line-height: .62rem
+        padding-left: .2rem
+        background: #fff
+        color: #666
 </style>
